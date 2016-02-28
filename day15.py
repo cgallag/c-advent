8b478c27b49563ffdd4fed6841804073d663c935
Caroline Gallagher <cagallagher2@gmail.com>
2016-02-28 00:48:01 -0500

    Day 15 in progress

--- day15.py
+++ day15.py
@@ -1,3 +1,5 @@
+import itertools
+
 
 def get_flavor_traits(flavor):
     name, attributes = flavor.split(":")
@@ -9,18 +11,21 @@ def get_flavor_traits(flavor):
 
     return flavor_dict
 
-def get_best_combination(flavor_data):
-    pass
 
-def get_best_cookie():
+def get_best_cookie(flavor_data):
+    ingredient_list = flavor_data.keys()
+
+
+def main():
     flavor_data = {}
     with open('data/day15.txt', 'r') as f:
         for line in f:
             flavor_data.update(get_flavor_traits(line))
 
-    return get_best_combination(flavor_data)
+    print 'The total score of the highest scoring cookie is {}'.format(get_best_cookie(flavor_data))
+
 
 if __name__ == "__main__":
-    get_best_cookie()
+    main()
 
 
